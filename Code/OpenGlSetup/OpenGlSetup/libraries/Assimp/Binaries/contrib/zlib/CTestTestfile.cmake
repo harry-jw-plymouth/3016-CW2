85489694e27b784aefb754b6999505a512492059
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2026-01-10 16:10:20 +0000

    Added open gl and assimp library to directory

---
+++ Code/OpenGlSetup/OpenGlSetup/libraries/Assimp/Binaries/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Public/Assimp/Source/assimp/contrib/zlib
+# Build directory: C:/Users/Public/Assimp/Binaries/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
